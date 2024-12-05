fef0555200fd56cfd212288fec6a25ec087b5ace
just-ctrlC-ctrlV <tanmay.shukla.mec21@itbhu.ac.in>
2024-12-06 02:49:27 +0530

    serevr written one man army

--- backend/controllers/eventcontroller.js
+++ backend/controllers/eventcontroller.js
@@ -1,20 +1,41 @@
+const db = require("../config/firebase");
+
 module.exports.createEvent = async function (req, res) {
   try {
-    res.json("Event Created");
+    const data = req.body;
+    await db.collection("events").add(data);
+    res.status(201).json("Event Created Sucessfully");
   } catch (error) {
     console.log(error.message);
   }
 };
 module.exports.getEvent = async function (req, res) {
   try {
-    res.json("get Event");
+    const useremail = req.body.email;
+    const snapsh = db.collection("events");
+    var ref = await snapsh.where("email", "==", `${useremail}`).get();
+    var list = [];
+    ref.forEach((doc) => {
+      list.push({ id: doc.id, data: doc.data() });
+    });
+
+    res.status(200).json({
+      message: "Get Event ",
+      list,
+    });
   } catch (error) {
     console.log(error.message);
   }
 };
 module.exports.getAllEvent = async function (req, res) {
   try {
-    res.json("get all Event");
+    const snapsh = db.collection("events");
+    var ref = await snapsh.get();
+    var list = [];
+    ref.forEach((doc) => {
+      list.push({ id: doc.id, data: doc.data() });
+    });
+    res.status(200).json(list);
   } catch (error) {
     console.log(error.message);
   }
